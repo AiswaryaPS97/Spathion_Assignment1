616b37132be51b5eb60ceeaec54d482fbd20f7d6
AiswaryaPS97 <82209042+AiswaryaPS97@users.noreply.github.com>
2021-04-10 21:20:37 +0530

    Add files via upload

---
+++ Greeter.sol
@@ -0,0 +1,24 @@
+pragma solidity >=0.7.0 < 0.9.0;
+
+contract Greeter
+{
+    
+	string private str;
+    
+	function set(string memory) private
+    
+	{
+        
+		str="hello world";
+    
+	}
+    
+	function get() private view returns (string memory)
+    
+	{
+        
+		return str;
+    
+	}
+
+}
